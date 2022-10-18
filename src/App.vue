675626001fe419ae8893854f8ce5262c7c56ed2e
侯帅 <houshuai0929@gmail.com>
2022-10-18 10:34:36 +0800

    '初始化完成'

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <van-button type="primary">主要按钮</van-button>
     <van-button type="info">信息按钮</van-button>
+    <van-icon name="chat-o" />
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/utils/vant'
 // 导入 vant 中所有的组件
-import Vant from 'vant';
+// import Vant from 'vant';
 import 'vant/lib/index.css';
 
-// vant 是作为 vue 的插件来使用的
-Vue.use(Vant)
+// // vant 是作为 vue 的插件来使用的
+// Vue.use(Vant)
+import { Button, Icon } from 'vant'
+
+Vue.use(Button)
+Vue.use(Icon)
 Vue.config.productionTip = false
 
 new Vue({
