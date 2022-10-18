773604018fc52ac8a16ccec29095f3a4e8319ce9
侯帅 <houshuai0929@gmail.com>
2022-10-18 09:54:45 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入 vant 中所有的组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
+// vant 是作为 vue 的插件来使用的
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
